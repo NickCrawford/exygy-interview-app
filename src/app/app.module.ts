7dd551fc5a3727251c3c59b20001bd7b79abab0d
NickCrawford <hi@nickcrawford.design>
2018-07-02 12:26:38 -0400

    Generates components for browse page and homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BrowseComponent } from './browse/browse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    BrowseComponent
   ],
   imports: [
     BrowserModule
