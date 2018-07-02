b6ff4a37115564a5f4447fbbf7065b2ffdd0cd2e
NickCrawford <hi@nickcrawford.design>
2018-07-02 19:11:38 -0400

    Populates document info templates with actual data

--- src/app/shared/slat-info/slat-info.component.ts
+++ src/app/shared/slat-info/slat-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-slat-info',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slat-info.component.scss']
 })
 export class SlatInfoComponent implements OnInit {
+
+  @Input() document: Document;
 
   constructor() { }
 
